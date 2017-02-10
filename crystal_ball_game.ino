e5bbd1a0f90a8f89c8512d716a335b80b8faa62b
irimina <irimina@gmail.com>
2017-02-10 15:44:37 -0500

    Add files via upload

---
+++ crystal_ball_game.ino
@@ -0,0 +1,62 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(8,9,4,5,6,7);
+#define my_button 0
+int switchPin=A0;//game
+int switchState=0;
+int prevSwitchState = 0;
+int reply;
+
+void setup() {
+  // put your setup code here, to run once:
+lcd.begin(16,2);
+pinMode(switchPin,INPUT);//game
+
+lcd.print("Ask the");
+lcd.setCursor(0,1);
+lcd.print("Crystal Ball!");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+switchState = digitalRead(switchPin);
+  if(switchState != prevSwitchState){
+    if(switchState ==LOW) {
+      reply = random(8);
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("The ball says: ");
+        lcd.setCursor(0,1);
+
+        switch(reply){
+         case 0:
+         lcd.print("Yes");
+         break;
+         case 1:
+         lcd.print("Most likely");
+         break;
+         case 2:
+         lcd.print("Certainly");
+         break;
+         case 3:
+         lcd.print("Outlook good");
+         break;
+         case 4:
+         lcd.print("Unsure");
+         break;
+         case 5:
+         lcd.print("Ask again");
+         break;
+         case 6:
+         lcd.print("Doubtful");
+         break;
+         case 7:
+         lcd.print("No");
+         break;      
+        }
+    }
+  }
+
+ prevSwitchState = switchState;
+}
